677bbddb95145dbd7c1d6a5cd2da40b98deb3934
Loyola User <Confer40@luc.edu>
2019-03-30 11:35:41 -0500

    adding individual page elements

---
+++ script1.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	  
+	$("#q1-brought").click(function(){
+	  alert("brought!!");
+	  
+	  $("#q2").text("You just finished your game, how long is your shower?");
+	});  
+	  
+	$("#q1-bought").click(function(){
+	  alert("bought!!");
+	   $("#q2").text("You just finished your game, how long is your shower?");
+	});   
+	  
+	
+	
+	
+	
+
+});
