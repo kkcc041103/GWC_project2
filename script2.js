340b2f60868e66aa40ff2d6af365b0d20603fada
Loyola User <Confer40@luc.edu>
2019-04-13 11:31:54 -0500

    4/13/19

---
+++ script2.js
@@ -0,0 +1,156 @@
+$(document).ready(function(){
+
+	  
+	$("#q1-:Landfills").click(function(){
+	  alert("Landfills!!");
+	  
+	  $("#q2").text("What affect do you think your actions have on animals?");
+	  var show = document.getElementById("q2-Loss-of-Biodiversity");
+	
+	 
+	 if (show.style.display === "none") {
+		 show.style.display = "block";
+	 }
+	 	  var show1= document.getElementById("q2-Loss-of-Habitat");
+	
+	 
+	 if (show1.style.display === "none") {
+		 show1.style.display = "block";
+	 }
+	 	  var show2 = document.getElementById("q2-Endangerment");
+	
+	 
+	 if (show2.style.display === "none") {
+		 show2.style.display = "block";
+	 }
+	});  
+	  
+	$("#q1-Oceans").click(function(){
+	  alert("Oceans!!");
+	   $("#q2").text("What affect do you think your actions have on animals?");
+	   var show = document.getElementById("q2-Loss-of-Biodiversity");
+	
+	 
+	 if (show.style.display === "none") {
+		 show.style.display = "block";
+	 }
+	 	  var show1= document.getElementById("q2-Loss-of-Habitat");
+	
+	 
+	 if (show1.style.display === "none") {
+		 show1.style.display = "block";
+	 }
+	 	  var show2 = document.getElementById("q2-Endangerment");
+	
+	 
+	 if (show2.style.display === "none") {
+		 show2.style.display = "block";
+	 }
+	}); 
+		$("#q1-Rivers").click(function(){
+	  alert("Rivers!!");
+	   $("#q2").text("What affect do you think your actions have on animals?");
+	    nvar show = document.getElementById("q2-5-Loss-of-Biodiversity");
+	
+	 
+	 if (show.style.display === "none") {
+		 show.style.display = "block";
+	 }
+	 	  var show1= document.getElementById("q2-Loss-of-Habitat");
+	
+	 
+	 if (show1.style.display === "none") {
+		 show1.style.display = "block";
+	 }
+	 	  var show2 = document.getElementById("q2-Endangerment");
+	
+	 
+	 if (show2.style.display === "none") {
+		 show2.style.display = "block";
+	 }
+	});
+	  $("#q2-Loss of Biodiversity").click(function(){
+	  alert("Biodiversity!!");
+	  console.log("hi");
+	  $("#q3").text("How many countries do you think support the Paris Agreement?");
+	var show = document.getElementById("Loss-of-Biodiversity");
+	
+	 
+	 if (show.style.display === "none") {
+		 show.style.display = "block";
+	 }
+	  });
+	 $("#q2-Loss-of-Habitat").click(function(){
+	  alert("mins!!");
+	  console.log("hi");
+	  $("#q3").text("How many countries do you think are involved in the Paris Agreement?");
+	var show = document.getElementById("q2-207");
+	
+	 
+	 if (show.style.display === "none") {
+		 show.style.display = "block";
+	 }
+	 	  var show1= document.getElementById("q2-117");
+	
+	 
+	 if (show1.style.display === "none") {
+		 show1.style.display = "block";
+	 }
+	 	  var show2 = document.getElementById("q2-195");
+	
+	 
+	 if (show2.style.display === "none") {
+		 show2.style.display = "block";
+	 }
+	  });
+	 $("#q2-Endangerment").click(function(){
+	  alert("mins!!");
+	  console.log("hi");
+	  $("#q3").text("How many countries do you think are involved in the Paris Agreement?");
+	var show = document.getElementById("q2-207");
+	
+	 
+	 if (show.style.display === "none") {
+		 show.style.display = "block";
+	 }
+	 	  var show1= document.getElementById("q2-117");
+	
+	 
+	 if (show1.style.display === "none") {
+		 show1.style.display = "block";
+	 }
+	 	  var show2 = document.getElementById("q2-195");
+	
+	 
+	 if (show2.style.display === "none") {
+		 show2.style.display = "block";
+	 }
+	  });
+   $("#q2-Endangerment").click(function(){
+	  alert("mins!!");
+	  console.log("hi");
+	  $("#q3").text("How many countries do you think are involved in the Paris Agreement?");
+	var show = document.getElementById("q2-207");
+	
+	 
+	 if (show.style.display === "none") {
+		 show.style.display = "block";
+	 }
+	 	  var show1= document.getElementById("q2-117");
+	S
+	 
+	 if (show1.style.display === "none") {
+		 show1.style.display = "block";
+	 }
+	 	  var show2 = document.getElementById("q2-195");
+	
+	 
+	 if (show2.style.display === "none") {
+		 show2.style.display = "block";
+	 }
+   });
+	 	 
+	 
+});
+
+	
