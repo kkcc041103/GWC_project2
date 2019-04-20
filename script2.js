0086dca139765971e2e2f3cdd9c1a19809936ece
CTSDH <ctsdh@CTSDHs-iMac.local>
2019-04-20 12:21:04 -0500

    4/20/2019

--- script2.js
+++ script2.js
@@ -1,156 +1,280 @@
 $(document).ready(function(){
 
-	  
-	$("#q1-:Landfills").click(function(){
-	  alert("Landfills!!");
-	  
-	  $("#q2").text("What affect do you think your actions have on animals?");
-	  var show = document.getElementById("q2-Loss-of-Biodiversity");
-	
-	 
-	 if (show.style.display === "none") {
-		 show.style.display = "block";
-	 }
-	 	  var show1= document.getElementById("q2-Loss-of-Habitat");
-	
-	 
-	 if (show1.style.display === "none") {
-		 show1.style.display = "block";
-	 }
-	 	  var show2 = document.getElementById("q2-Endangerment");
-	
-	 
-	 if (show2.style.display === "none") {
-		 show2.style.display = "block";
-	 }
-	});  
-	  
-	$("#q1-Oceans").click(function(){
-	  alert("Oceans!!");
-	   $("#q2").text("What affect do you think your actions have on animals?");
-	   var show = document.getElementById("q2-Loss-of-Biodiversity");
-	
-	 
-	 if (show.style.display === "none") {
-		 show.style.display = "block";
-	 }
-	 	  var show1= document.getElementById("q2-Loss-of-Habitat");
-	
-	 
-	 if (show1.style.display === "none") {
-		 show1.style.display = "block";
-	 }
-	 	  var show2 = document.getElementById("q2-Endangerment");
-	
-	 
-	 if (show2.style.display === "none") {
-		 show2.style.display = "block";
-	 }
-	}); 
-		$("#q1-Rivers").click(function(){
-	  alert("Rivers!!");
-	   $("#q2").text("What affect do you think your actions have on animals?");
-	    nvar show = document.getElementById("q2-5-Loss-of-Biodiversity");
-	
-	 
-	 if (show.style.display === "none") {
-		 show.style.display = "block";
-	 }
-	 	  var show1= document.getElementById("q2-Loss-of-Habitat");
-	
-	 
-	 if (show1.style.display === "none") {
-		 show1.style.display = "block";
-	 }
-	 	  var show2 = document.getElementById("q2-Endangerment");
-	
-	 
-	 if (show2.style.display === "none") {
-		 show2.style.display = "block";
-	 }
-	});
-	  $("#q2-Loss of Biodiversity").click(function(){
-	  alert("Biodiversity!!");
-	  console.log("hi");
-	  $("#q3").text("How many countries do you think support the Paris Agreement?");
-	var show = document.getElementById("Loss-of-Biodiversity");
-	
-	 
-	 if (show.style.display === "none") {
-		 show.style.display = "block";
-	 }
-	  });
-	 $("#q2-Loss-of-Habitat").click(function(){
-	  alert("mins!!");
-	  console.log("hi");
-	  $("#q3").text("How many countries do you think are involved in the Paris Agreement?");
-	var show = document.getElementById("q2-207");
-	
-	 
-	 if (show.style.display === "none") {
-		 show.style.display = "block";
-	 }
-	 	  var show1= document.getElementById("q2-117");
-	
-	 
-	 if (show1.style.display === "none") {
-		 show1.style.display = "block";
-	 }
-	 	  var show2 = document.getElementById("q2-195");
-	
-	 
-	 if (show2.style.display === "none") {
-		 show2.style.display = "block";
-	 }
-	  });
-	 $("#q2-Endangerment").click(function(){
-	  alert("mins!!");
-	  console.log("hi");
-	  $("#q3").text("How many countries do you think are involved in the Paris Agreement?");
-	var show = document.getElementById("q2-207");
-	
-	 
-	 if (show.style.display === "none") {
-		 show.style.display = "block";
-	 }
-	 	  var show1= document.getElementById("q2-117");
-	
-	 
-	 if (show1.style.display === "none") {
-		 show1.style.display = "block";
-	 }
-	 	  var show2 = document.getElementById("q2-195");
-	
-	 
-	 if (show2.style.display === "none") {
-		 show2.style.display = "block";
-	 }
-	  });
-   $("#q2-Endangerment").click(function(){
-	  alert("mins!!");
-	  console.log("hi");
-	  $("#q3").text("How many countries do you think are involved in the Paris Agreement?");
-	var show = document.getElementById("q2-207");
-	
-	 
-	 if (show.style.display === "none") {
-		 show.style.display = "block";
-	 }
-	 	  var show1= document.getElementById("q2-117");
-	S
-	 
-	 if (show1.style.display === "none") {
-		 show1.style.display = "block";
-	 }
-	 	  var show2 = document.getElementById("q2-195");
-	
-	 
-	 if (show2.style.display === "none") {
-		 show2.style.display = "block";
-	 }
-   });
-	 	 
-	 
-});
 
-	
+		$("#q1-:Landfills").click(function(){
+				alert("Landfills!!");
+
+				$("#q2").text("What affect do you think your actions have on animals?");
+				var show = document.getElementById("q2-Loss-of-Biodiversity");
+
+				if (show.style.display === "none") {
+				show.style.display = "block";
+				}
+				var show1= document.getElementById("q2-Loss-of-Habitat");
+
+
+				if (show1.style.display === "none") {
+				show1.style.display = "block";
+				}
+				var show2 = document.getElementById("q2-Endangerment");
+
+
+				if (show2.style.display === "none") {
+				show2.style.display = "block";
+				}
+		});
+
+		$("#q1-Oceans").click(function(){
+				alert("Oceans!!");
+				$("#q2").text("What affect do you think your actions have on animals?");
+				var show = document.getElementById("q2-Loss-of-Biodiversity");
+
+
+				if (show.style.display === "none") {
+				show.style.display = "block";
+				}
+				var show1= document.getElementById("q2-Loss-of-Habitat");
+
+
+				if (show1.style.display === "none") {
+				show1.style.display = "block";
+				}
+				var show2 = document.getElementById("q2-Endangerment");
+
+
+				if (show2.style.display === "none") {
+				show2.style.display = "block";
+				}
+		});
+
+		$("#q1-Rivers").click(function(){
+				alert("Rivers!!");
+				$("#q2").text("What affect do you think your actions have on animals?");
+				var show = document.getElementById("q2-Loss-of-Biodiversity");
+
+
+				if (show.style.display === "none") {
+				show.style.display = "block";
+				}
+				var show1= document.getElementById("q2-Loss-of-Habitat");
+
+
+				if (show1.style.display === "none") {
+				show1.style.display = "block";
+				}
+				var show2 = document.getElementById("q2-Endangerment");
+
+
+				if (show2.style.display === "none") {
+				show2.style.display = "block";
+				}
+		});
+
+		$("#q2-Loss of Biodiversity").click(function(){
+				alert("Biodiversity!!");
+				console.log("hi");
+				$("#q3").text("How many countries do you think support the Paris Agreement?");
+				var show = document.getElementById("q3-201-countries");
+
+
+				if (show.style.display === "none") {
+				show.style.display = "block";
+				}
+				var show1= document.getElementById("q3-117-countries");
+
+
+				if (show1.style.display === "none") {
+				show1.style.display = "block";
+				}
+				var show2= document.getElementById("q3-195-countries");
+
+
+				if (show2.style.display === "none") {
+				show2.style.display = "block";
+				}
+		});
+
+		$("#q2-Loss-of-Habitat").click(function(){
+				alert("mins!!");
+				console.log("hi");
+				$("#q3").text("How many countries do you think are involved in the Paris Agreement?");
+				var show4 = document.getElementById("q3-201-countries");
+
+
+				if (show4.style.display === "none") {
+				show4.style.display = "block";
+				}
+				var show1= document.getElementById("q3-117-countries");
+
+
+				if (show1.style.display === "none") {
+				show1.style.display = "block";
+				}
+				var show2 = document.getElementById("q3-195-countries");
+
+
+				if (show2.style.display === "none") {
+				show2.style.display = "block";
+				}
+		});
+
+		$("#q2-Endangerment").click(function(){
+				alert("mins!!");
+				console.log("hi");
+				$("#q3").text("How many countries do you think are involved in the Paris Agreement?");
+				var show = document.getElementById("q3-201-countries");
+
+
+				if (show.style.display === "none") {
+				show.style.display = "block";
+				}
+				var show1= document.getElementById("q3-117-countries");
+
+
+				if (show1.style.display === "none") {
+				show1.style.display = "block";
+				}
+				var show2 = document.getElementById("q3-195-countries");
+
+
+				if (show2.style.display === "none") {
+				show2.style.display = "block";
+				}
+		});
+
+
+		$("#q3-201-countries").click(function(){
+				alert("mins!!");
+				console.log("hi");
+				$("#q4").text("How much time do you think your food takes to get to your plate?");
+				var show = document.getElementById("q4-1500-miles");
+
+
+				if (show.style.display === "none") {
+				show.style.display = "block";
+				}
+				var show1= document.getElementById("q4-5000-miles");
+
+				if (show1.style.display === "none") {
+				show1.style.display = "block";
+				}
+				var show2 = document.getElementById("q4-10000-miles");
+
+
+				if (show2.style.display === "none") {
+				show2.style.display = "block";
+				}
+		});
+
+		$("#q3-117-countries").click(function(){
+				alert("mins!!");
+				console.log("hi");
+				$("#q4").text("How far does your food gravel to get to your plate?");
+				var show = document.getElementById("q4-1500-miles");
+
+
+				if (show.style.display === "none") {
+				show.style.display = "block";
+				}
+				var show1= document.getElementById("q4-5000-miles");
+
+				if (show1.style.display === "none") {
+				show1.style.display = "block";
+				}
+				var show2 = document.getElementById("q4-10000-miles");
+
+
+				if (show2.style.display === "none") {
+				show2.style.display = "block";
+				}
+		});
+
+			$("#q3-195-countries").click(function(){
+					alert("mins!!");
+					console.log("hi");
+					$("#q4").text("How much time do you think your food takes to get to your plate?");
+					var show = document.getElementById("q4-1500-miles");
+
+
+					if (show.style.display === "none") {
+					show.style.display = "block";
+					}
+					var show1= document.getElementById("q4-5000-miles");
+
+					if (show1.style.display === "none") {
+					show1.style.display = "block";
+					}
+					var show2 = document.getElementById("q4-10000-miles");
+
+
+					if (show2.style.display === "none") {
+					show2.style.display = "block";
+					}
+			});
+			$("#q4-1500-miles").click(function(){
+					alert("mins!!");
+					console.log("hi");
+					$("#q5").text("Where does your food go after you dispose it?");
+					var show = document.getElementById("q5-Dumpster");
+
+
+					if (show.style.display === "none") {
+					show.style.display = "block";
+					}
+					var show1= document.getElementById("q5-Compost");
+
+					if (show1.style.display === "none") {
+					show1.style.display = "block";
+					}
+					var show2 = document.getElementById("q5-Sewers");
+
+
+					if (show2.style.display === "none") {
+					show2.style.display = "block";
+					}
+			});
+			$("#q4-5000-miles").click(function(){
+					alert("mins!!");
+					console.log("hi");
+					$("#q5").text("Where does your food go after you dispose it?");
+					var show = document.getElementById("q5-Dumpster");
+
+
+					if (show.style.display === "none") {
+					show.style.display = "block";
+					}
+					var show1= document.getElementById("q5-Compost");
+
+					if (show1.style.display === "none") {
+					show1.style.display = "block";
+					}
+					var show2 = document.getElementById("q5-Sewers");
+
+
+					if (show2.style.display === "none") {
+					show2.style.display = "block";
+					}
+			});
+			$("#q4-10000-miles").click(function(){
+					alert("mins!!");
+					console.log("hi");
+					$("#q5").text("Where does your food go after you dispose it?");
+					var show = document.getElementById("q5-Dumpster");
+
+					if (show.style.display === "none") {
+					show.style.display = "block";
+					}
+					var show1= document.getElementById("q5-Compost");
+
+					if (show1.style.display === "none") {
+					show1.style.display = "block";
+					}
+					var show2 = document.getElementById("q5-Sewers");
+
+
+					if (show2.style.display === "none") {
+					show2.style.display = "block";
+					}
+			});
+			
+});
